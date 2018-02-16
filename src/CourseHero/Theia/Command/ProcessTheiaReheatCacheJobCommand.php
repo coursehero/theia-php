327e1a058f376f060a9b72090b08b0cbff53295f
Kevin Wilde <kevin.wilde@coursehero.com>
2018-02-16 15:55:51 -0800

    chess-213 implement job handlers for theia cache reheat jobs

--- src/CourseHero/Theia/Command/ProcessTheiaReheatCacheJobCommand.php
+++ src/CourseHero/Theia/Command/ProcessTheiaReheatCacheJobCommand.php
@@ -6,19 +6,35 @@ use CourseHero\QueueBundle\Component\QueueInterface;
 use CourseHero\QueueBundle\Component\QueueMessageInterface;
 use CourseHero\QueueBundle\Constant\Queue;
 use CourseHero\QueueBundle\Service\QueueService;
+use CourseHero\StudyGuideBundle\Constants\StageConstants;
+use CourseHero\StudyGuideBundle\Service\StudyGuideConnectionService;
+use CourseHero\TheiaBundle\Service\TheiaProviderService;
 use CourseHero\UtilsBundle\Command\AbstractPerpetualCommand;
 use CourseHero\UtilsBundle\Service\SlackMessengerService;
+use JMS\Serializer\SerializerBuilder;
+use JMS\SerializerBundle\JMSSerializerBundle;
+use StudyGuideBlocks\Blocks\Block;
+use StudyGuideBlocks\Blocks\CourseBlock;
+use StudyGuideBlocks\Blocks\SectionBlock;
+use StudyGuideBlocks\Blocks\SubtopicBlock;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class TheiaJobHandler
 {
-    protected $componentLibrary;
+    /** @var string */
+    public static $componentLibrary;
+
+    /** @var QueueInterface */
     protected $queue;
 
-    public function __construct(QueueInterface $queue)
+    /** @var \Theia\Client */
+    protected $theiaClient;
+
+    public function __construct(QueueInterface $queue, \Theia\Client $theiaClient)
     {
         $this->queue = $queue;
+        $this->theiaClient = $theiaClient;
     }
 
     protected function createRenderJob(string $component, string $props)
@@ -32,7 +48,7 @@ abstract class TheiaJobHandler
                 ],
                 'ComponentLibrary' => [
                     'DataType' => 'String',
-                    'StringValue' => $this->componentLibrary,
+                    'StringValue' => self::$componentLibrary,
                 ],
                 'Component' => [
                     'DataType' => 'String',
@@ -45,11 +61,7 @@ abstract class TheiaJobHandler
 
     protected function createProducerJob(string $producerGroup)
     {
-        $message = $this->queue->createMessage(
-            [
-                'producerGroup' => $producerGroup,
-            ]
-        );
+        $message = $this->queue->createMessage(['producerGroup' => $producerGroup]);
         $message->setAttributes(
             [
                 'Type' => [
@@ -58,7 +70,7 @@ abstract class TheiaJobHandler
                 ],
                 'ComponentLibrary' => [
                     'DataType' => 'String',
-                    'StringValue' => $this->componentLibrary,
+                    'StringValue' => self::$componentLibrary,
                 ],
             ]
         );
@@ -72,25 +84,92 @@ abstract class TheiaJobHandler
 
 class StudyGuideTheiaJobHandler extends TheiaJobHandler
 {
-    public function __construct(QueueInterface $queue)
+    /** @var StudyGuideConnectionService */
+    private $studyGuideConnectionService;
+
+    /** @var JMSSerializerBundle */
+    private $jmsSerializer;
+
+    public function __construct(QueueInterface $queue, \Theia\Client $theiaClient, StudyGuideConnectionService $studyGuideConnectionService)
     {
-        parent::__construct($queue);
-        $this->componentLibrary = '@coursehero-components/study-guides';
+        parent::__construct($queue, $theiaClient);
+        $this->studyGuideConnectionService = $studyGuideConnectionService;
+        $this->jmsSerializer = SerializerBuilder::create()->build();
+        self::$componentLibrary = '@coursehero-components/study-guides';
     }
 
+    /**
+     * @param string $builtAt
+     * @param string $commitHash
+     * @throws \Exception
+     */
     public function processNewBuildJob(string $builtAt, string $commitHash)
     {
-        // get all published course names
+        $courseBlocks = $this->studyGuideConnectionService->getCoursesTree(StageConstants::STAGE_PUBLISHED);
+        $this->createRenderJob('IndexApp', $this->jmsSerializer->serialize(['courses' => $courseBlocks], 'json'));
 
-        // make producer job for each one
-        $this->createProducerJob('Biology');
-        $this->createProducerJob('Biology 2');
-        $this->createProducerJob('Biology 3');
+        /** @var CourseBlock $courseBlock */
+        foreach ($courseBlocks as $courseBlock) {
+            $this->createProducerJob($courseBlock->getName());
+        }
     }
 
+    /**
+     * @param string $producerGroup
+     * @throws \Exception
+     */
     public function processProducerJob(string $producerGroup)
     {
-        $this->createRenderJob('IndexApp', '{}');
+        $courseTree = $this->studyGuideConnectionService->getCourseTree($producerGroup, StageConstants::STAGE_PUBLISHED);
+        /** @var Block $block */
+        foreach ($courseTree->createIterator() as $block) {
+            if (in_array($block->getBlockType(), [SectionBlock::BLOCK_TYPE, SubtopicBlock::BLOCK_TYPE])) {
+                $props = $this->jmsSerializer->serialize(["course" => $courseTree, "location" => $this->route($block)], 'json');
+                $this->theiaClient->renderAndCache(self::$componentLibrary, 'CourseApp', $props, true);
+            }
+        }
+    }
+
+    /**
+     * Needs to match implementation in components/study-guides/src/utils/route.js
+     *
+     * @param Block $block
+     * @return string
+     */
+    private function route(Block $block): string
+    {
+        $slug = function (string $string): string {
+            $removePatterns = [
+                '\?',
+                // '\(',
+                // '\)',
+                ','
+            ];
+            $removePatternsRegex = '/' . join('|', $removePatterns) . '/';
+            $spacesReplaced = preg_replace('/\s/', '-', $string);
+            $charsRemoved = preg_replace($removePatternsRegex, '', $spacesReplaced);
+            return strtolower($charsRemoved);
+        };
+
+        $createUrl = function (string $courseSlug, string $subtopicSlug, string $sectionSlug): string {
+            if ($sectionSlug === 'overview') {
+                $sectionSlug = '';
+            }
+            $parts = array_filter(['study-guides', $courseSlug, $subtopicSlug, $sectionSlug]);
+            return '/' . join('/', $parts) . '/';
+        };
+
+        $blockLevels = [$block];
+        while ($block->getParent()) {
+            $block = $block->getParent();
+            array_unshift($blockLevels, $block);
+        }
+
+        $courseSlug = $slug($blockLevels[0]->getName());
+        $subtopicSlug = isset($blockLevels[2]) ? $slug($blockLevels[2]->getName()) : '';
+        $sectionSlug = isset($blockLevels[3]) ? $slug($blockLevels[3]->getName()) : '';
+
+        return $createUrl($courseSlug, $subtopicSlug, $sectionSlug);
     }
 }
 
@@ -107,6 +186,9 @@ class ProcessTheiaReheatCacheJobCommand extends AbstractPerpetualCommand
     /** @var  QueueInterface */
     protected $queue;
 
+    /** @var \Theia\Client */
+    protected $theiaClient;
+
     /** @var array */
     protected $handlers;
 
@@ -118,11 +200,12 @@ class ProcessTheiaReheatCacheJobCommand extends AbstractPerpetualCommand
 
     public function singleRun(InputInterface $input, OutputInterface $output)
     {
-        // $this->write('Starting to process next Theia job');
         $queue = $this->getQueue();
+        $theiaClient = $this->getTheiaClient();
+        $studyGuideConnectionService = $this->getStudyGuideConnectionService();
 
         $this->handlers = [
-            '@coursehero-components/study-guides' => new StudyGuideTheiaJobHandler($queue),
+            StudyGuideTheiaJobHandler::$componentLibrary => new StudyGuideTheiaJobHandler($queue, $theiaClient, $studyGuideConnectionService),
         ];
 
         try {
@@ -130,22 +213,18 @@ class ProcessTheiaReheatCacheJobCommand extends AbstractPerpetualCommand
             if (!$message) {
                 return; // if no more messages just return and restart for now
             }
+            // Delete message from queue so long jobs don't get processed multiple times
+            $queue->deleteMessage($message);
 
             $this->write("Job processing started");
             $this->processJob($message);
-            $this->write("Finished job");
-            // $queue->deleteMessage($message);
+            $this->write("Finished job\n");
         } catch (\Exception $e) {
-            // if no message we encountered an error getting a message
-            if (!isset($message)) {
-                // TODO ?
-                // $this->getSlackMessengerService()->send("Exception: {$e->getMessage()}", '#csi-debug', 'dmca-bot');
-                return;
+            if (isset($message)) {
+                $queue->sendMessage($message);
             }
-
-            // if we didn't successfully process the message, requeue and release 'lock' on request
-            // $queue->createMessage($message->getBody());
-            // $this->getSlackMessengerService()->send("Exception processing {$message->getBody()}: {$e->getMessage()}", '#csi-debug', 'dmca-bot');
+            $this->write("Job failed {$e->getMessage()}");
+            $this->sendSlackMessage("Exception: {$e->getMessage()}");
         }
     }
 
@@ -163,6 +242,10 @@ class ProcessTheiaReheatCacheJobCommand extends AbstractPerpetualCommand
         return $queue->receiveMessage() ?: null;
     }
 
+    /**
+     * @param QueueMessageInterface $message
+     * @throws \Exception
+     */
     protected function processJob(QueueMessageInterface $message)
     {
         $body = $message->getBody();
@@ -171,34 +254,37 @@ class ProcessTheiaReheatCacheJobCommand extends AbstractPerpetualCommand
         $jobHandler = $this->handlers[$componentLibrary];
         $type = $attrs['Type']['StringValue'];
 
-        $this->write("Processing $type");
+        $attrsAsString = json_encode($attrs, JSON_PRETTY_PRINT);
+        $this->write("Processing $type with $attrsAsString");
+        if ($type !== 'render-job') {
+            $bodyAsString = json_encode($body, JSON_PRETTY_PRINT);
+            $this->write("Body: $bodyAsString");
+        }
 
-        if ($type === 'render-job') {
-            $props = json_encode($body); // TODO $message->body is already deserialized into an object - this is a waste of processing for our use case.
-            $this->processRenderJob($componentLibrary, $attrs['Component']['StringValue'], $props);
-        } elseif ($type === 'producer-job') {
-            $jobHandler->processProducerJob($body['producerGroup']);
-        } elseif ($type === 'new-build-job') {
+        if ($type === 'new-build-job') {
             $jobHandler->processNewBuildJob($body['builtAt'], $body['commitHash']);
+        } elseif ($type === 'producer-job') {
+            $jobHandler->processProducerJob($body['producerGroup'], $body);
+        } elseif ($type === 'render-job') {
+            $props = json_encode($body); // TODO $message->body is already deserialized into an object - this is a waste of processing for our use case.
+            $component = $attrs['Component']['StringValue'];
+            $this->processRenderJob($componentLibrary, $component, $props);
         } else {
             throw new \Exception("unexpected job type: $type");
         }
     }
 
-    protected function processRenderJob(string $componentLibrary, string $component, string $propsAsJson)
+    protected function processRenderJob(string $componentLibrary, string $component, string $props)
     {
-        $this->write("got a job ... \n");
-
-        // TODO make theia request here
+        $this->getTheiaClient()->renderAndCache($componentLibrary, $component, $props, true);
     }
 
     protected function getQueue(): QueueInterface
     {
-        if ($this->queue) {
-            return $this->queue;
+        if (!$this->queue) {
+            $queueName = $this->getQueueName();
+            $this->queue = $this->getQueueService()->getQueue($queueName);
         }
-        $queueName = $this->getQueueName();
-        $this->queue = $this->getQueueService()->getQueue($queueName);
 
         return $this->queue;
     }
@@ -210,9 +296,42 @@ class ProcessTheiaReheatCacheJobCommand extends AbstractPerpetualCommand
         return 'production' === $environment ? Queue::THEIA_REHEAT_JOBS : Queue::THEIA_REHEAT_JOBS_DEV;
     }
 
+    protected function getTheiaClient(): \Theia\Client
+    {
+        if (!$this->theiaClient) {
+            $this->theiaClient = $this->getTheiaProviderService()->getClient();
+        }
+
+        return $this->theiaClient;
+    }
+
+    protected function sendSlackMessage(string $text)
+    {
+        if ($this->getContainer()->getParameter('environment') !== 'localhost') {
+            $this->getSlackMessengerService()->send($text, $this->getSlackChannelName(), 'IRIS', ':eye:');
+        }
+    }
+
+    protected function getSlackChannelName(): string
+    {
+        $environment = $this->getContainer()->getParameter('environment');
+
+        return 'production' === $environment ? '#theia-errors-prod' : '#theia-errors-dev';
+    }
+
     protected function getQueueService(): QueueService
     {
         return $this->getContainer()->get(QueueService::SERVICE_ID);
+    }
+
+    protected function getStudyGuideConnectionService(): StudyGuideConnectionService
+    {
+        return $this->getContainer()->get(StudyGuideConnectionService::SERVICE_ID);
+    }
+
+    protected function getTheiaProviderService(): TheiaProviderService
+    {
+        return $this->getContainer()->get(TheiaProviderService::SERVICE_ID);
     }
 
     protected function getSlackMessengerService(): SlackMessengerService
