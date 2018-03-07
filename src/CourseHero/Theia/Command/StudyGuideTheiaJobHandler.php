5c045673df0b7e31de6e91c9cf478320fc40a041
Connor Clark <connor.clark@coursehero.com>
2018-03-07 00:49:40 -0800

    fix sg caching

--- src/CourseHero/Theia/Command/StudyGuideTheiaJobHandler.php
+++ src/CourseHero/Theia/Command/StudyGuideTheiaJobHandler.php
@@ -15,6 +15,20 @@ class StudyGuideTheiaJobHandler extends TheiaJobHandler
 {
     public static $componentLibrary = '@coursehero-components/study-guides';
 
+    /**
+     * @param CourseBlock $courseBlock
+     * @param Block $blockToRender
+     * @return string
+     */
+    public static function getProps(CourseBlock $courseBlock, Block $blockToRender)
+    {
+        $jmsSerializer = SerializerBuilder::create()->build();
+        return $jmsSerializer->serialize([
+            "course" => $courseBlock,
+            "blockToRenderId" => $blockToRender->getId()
+        ], 'json');
+    }
+
     /** @var StudyGuideConnectionService */
     private $studyGuideConnectionService;
 
@@ -37,7 +51,8 @@ class StudyGuideTheiaJobHandler extends TheiaJobHandler
     {
         $courseBlocks = $this->studyGuideConnectionService->getCoursesTree(StageConstants::STAGE_PUBLISHED);
         // TODO this is causing the body to be serialized twice, because it will be serialized again when the message is sent
-        $this->createRenderJob('IndexApp', $this->jmsSerializer->serialize(['courses' => $courseBlocks], 'json'));
+        // TODO commented out b/c we currently do not even use IndexApp
+        // $this->createRenderJob('IndexApp', $this->jmsSerializer->serialize(['courses' => $courseBlocks], 'json'));
 
         /** @var CourseBlock $courseBlock */
         foreach ($courseBlocks as $courseBlock) {
@@ -55,52 +70,9 @@ class StudyGuideTheiaJobHandler extends TheiaJobHandler
         /** @var Block $block */
         foreach ($courseTree->createIterator() as $block) {
             if (in_array($block->getBlockType(), [SectionBlock::BLOCK_TYPE, SubtopicBlock::BLOCK_TYPE])) {
-                $props = $this->jmsSerializer->serialize(["course" => $courseTree, "location" => $this->route($block)], 'json');
+                $props = self::getProps($courseTree, $block);
                 $this->theiaClient->renderAndCache(self::$componentLibrary, 'CourseApp', $props, true);
             }
         }
-    }
-
-    /**
-     * Needs to match implementation in components/study-guides/src/utils/route.js
-     *
-     * @param Block $block
-     * @return string
-     */
-    private function route(Block $block): string
-    {
-        $slug = function (string $string): string {
-            $removePatterns = [
-                '\?',
-                // '\(',
-                // '\)',
-                ',',
-                '\/'
-            ];
-            $removePatternsRegex = '/' . join('|', $removePatterns) . '/';
-            $spacesReplaced = preg_replace('/\s/', '-', $string);
-            $charsRemoved = preg_replace($removePatternsRegex, '', $spacesReplaced);
-            return strtolower($charsRemoved);
-        };
-
-        $createUrl = function (string $courseSlug, string $subtopicSlug, string $sectionSlug): string {
-            if ($sectionSlug === 'overview') {
-                $sectionSlug = '';
-            }
-            $parts = array_filter(['study-guides', $courseSlug, $subtopicSlug, $sectionSlug]);
-            return '/' . join('/', $parts) . '/';
-        };
-
-        $blockLevels = [$block];
-        while ($block->getParent()) {
-            $block = $block->getParent();
-            array_unshift($blockLevels, $block);
-        }
-
-        $courseSlug = $slug($blockLevels[0]->getName());
-        $subtopicSlug = isset($blockLevels[2]) ? $slug($blockLevels[2]->getName()) : '';
-        $sectionSlug = isset($blockLevels[3]) ? $slug($blockLevels[3]->getName()) : '';
-
-        return $createUrl($courseSlug, $subtopicSlug, $sectionSlug);
     }
 }
