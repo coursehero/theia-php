667930f7c3e5a80e94f469b3a6f035ba085776de
Kevin Wilde <kevin.wilde@coursehero.com>
2018-04-26 13:15:13 -0700

    chess-288 cache reheating for sg index page

--- src/CourseHero/Theia/Command/TheiaJobHandler.php
+++ src/CourseHero/Theia/Command/TheiaJobHandler.php
@@ -10,32 +10,9 @@ abstract class TheiaJobHandler
     /** @var \Theia\Client */
     protected $theiaClient;
 
-    /** @var ReheatCacheJobCreator */
-    protected $jobCreator;
-
-    public function __construct(\Theia\Client $theiaClient, ReheatCacheJobCreator $jobCreator)
+    public function __construct(\Theia\Client $theiaClient)
     {
         $this->theiaClient = $theiaClient;
-        $this->jobCreator = $jobCreator;
-    }
-
-    /**
-     * @param string $producerGroup
-     * @throws \Exception
-     */
-    protected function createProducerJob(string $producerGroup)
-    {
-        $this->jobCreator->createProducerJob(static::$componentLibrary, $producerGroup);
-    }
-
-    /**
-     * @param string $component
-     * @param string $props
-     * @throws \Exception
-     */
-    protected function createRenderJob(string $component, string $props)
-    {
-        $this->jobCreator->createRenderJob(static::$componentLibrary, $component, $props);
     }
 
     abstract public function processNewBuildJob(string $builtAt, string $commitHash);
